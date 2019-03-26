271364972a91fbc98464958592ce1054bf7e5dd0
Thomas Johnson <tjohnson1106@gmail.com>
2019-03-25 21:05:08 -0400

    app registry setup

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { AppRegistry } from 'react-native'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// register the app
+AppRegistry.registerComponent("App", () => App)
+
+// run the app
+AppRegistry.runApplication("App", {
+    initialProps: {},
+    rootTag:  document.getElementById("root")
+})
+
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
